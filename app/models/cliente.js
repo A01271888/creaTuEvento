14825640920a052471b41fb606054e5f1faf26a4
WEB <web@iMac-de-administrador-11.local>
2017-11-27 19:09:11 -0600

    Inicio de Proyecto

--- app/models/cliente.js
+++ app/models/cliente.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  nombre: DS.attr('string'),
+  telefono: DS.attr('string'),
+  mail: DS.attr('string'),
 
+  eventos: DS.hasMany('registro-evento')
 });
